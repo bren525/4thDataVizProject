6f29b72b571638dab0f8ee48cf6a37e064bf552b
jlangowitz <jlangowitz@gmail.com>
2015-11-23 02:07:25 -0500

    cleaning up duplicate code

--- mortality.js
+++ mortality.js
@@ -4,6 +4,32 @@ GLOBAL.filters = {
 	cause: null,
 	year: null
 }
+GLOBAL.categories = {
+	age: {
+		title: "Number of Deaths per Age Group",
+		xlabel: "Number of Deaths",
+		ylabel: "Age Group",
+		filter: null,
+		color: "blue",
+		excludeColor: "lightblue"
+	},
+	year: {
+		title: "Number of Deaths per Year",
+		xlabel: "Number of Deaths",
+		ylabel: "Year",
+		filter: null,
+		color: "red",
+		excludeColor: "maroon"
+	},
+	cause: {
+		title: "Number of Deaths per Cause of Death",
+		xlabel: "Number of Deaths",
+		ylabel: "Cause of Death",
+		filter: null,
+		color: "green",
+		excludeColor: "lime"
+	}
+}
 window.addEventListener("load", run);
 
 function run () {
@@ -17,97 +43,56 @@ function run () {
 function draw (svg) {
 	d3.select("#"+svg).selectAll("g").remove();
 
-	var cause_data = getCauseData();
-	var causeColors = [];
-	for (var i in cause_data.values) {
-		if (GLOBAL.filters.cause != null && GLOBAL.filters.cause != cause_data[i]) {
-			causeColors.push("#7ec0ee");
-		} else {
-			causeColors.push("blue");
+	for (category in GLOBAL.categories) {
+		var data = getData(category);
+		var colors = [];
+		for (var i in data.values) {
+			if (GLOBAL.categories[category].filter != null && GLOBAL.categories[category].filter != data[i]) {
+				colors.push(GLOBAL.categories[category].excludeColor);
+			} else {
+				colors.push(GLOBAL.categories[category].color);
+			}
 		}
+		var viz = viz_lib.bar_graph(svg, 100, 100, 700, 400, data, colors, .2);
+		viz.selectAll("rect").on("click", applyFilter(category));
 	}
-	var cause_breakdown = viz_lib.bar_graph(svg, 100, 100, 700, 400, cause_data, causeColors, .2);
-	cause_breakdown.selectAll("rect").on("click", filterCause);
-
-	var age_data = getAgeData();
-	var ageColors = [];
-	for (var i in age_data.values) {
-		if (GLOBAL.filters.age != null && GLOBAL.filters.age != age_data[i]) {
-			ageColors.push("#7ec0ee");
-		} else {
-			ageColors.push("blue");
-		}
-	}
-	var age_breakdown = viz_lib.bar_graph(svg, 100, 600, 700, 400, age_data, ageColors, .2);
-
-	var year_data = getYearData();
-	var yearColors = [];
-	for (var i in year_data.values) {
-		if (GLOBAL.filters.year != null && GLOBAL.filters.year != year_data[i]) {
-			yearColors.push("#7ec0ee");
-		} else {
-			yearColors.push("blue");
-		}
-	}
-	var year_breakdown = viz_lib.bar_graph(svg, 100, 1100, 700, 400, year_data, yearColors, .2);
 }
 
 function getData(category) {
-	var values_obj = {};
+	var data = {};
+	data.xlabel = GLOBAL.categories[category].xlabel;
+	data.ylabel = GLOBAL.categories[category].ylabel;
+	data.title = GLOBAL.categories[category].title;
+	var values = {};
 	for (i in GLOBAL.data) {
 		var row = GLOBAL.data[i]
-		if (isIncluded(row)) {
-			values_obj[row[category]] = (values_obj[row[category]] || 0) + row.count;
+		if (isIncluded(row, category)) {
+			values[row[category]] = (values[row[category]] || 0) + row.count;
 		}
 	}
-	var values = [];
-	for (var key in values_obj) {
-		values.push({label: key, val: values_obj[key]});
+	data.values = [];
+	for (var key in values) {
+		values.push({label: key, val: values[key]});
 	}
-	return values;
-}
-
-function getCauseData() {
-	var data = {};
-	data.xlabel = "Cause of Death";
-	data.ylabel = "Number of Deceased";
-	data.title = "Number of Deaths per Cause of Death";
-	data.values = getData("cause");
 	return data;
 }
 
-function getAgeData() {
-	var data = {};
-	data.xlabel = "Age Group";
-	data.ylabel = "Number of Deceased";
-	data.title = "Number of Deaths per Age Group";
-	data.values = getData("age");
-	return data;
-}
-
-function getYearData() {
-	var data = {};
-	data.xlabel = "Year of Death";
-	data.ylabel = "Number of Deceased";
-	data.title = "Number of Deaths per Year of Death";
-	data.values = getData("year");
-	return data;
-}
-
-function isIncluded(row) {
-	for (var filter in GLOBAL.filters) {
-		if (GLOBAL.filters[filter] != null && GLOBAL.filters[filter] != row[filter]) {
+function isIncluded(row, category) {
+	for (var cat in GLOBAL.categories) {
+		if (category != cat && GLOBAL.categories[cat].filter !,= null && GLOBAL.categories[cat].filter != row[cat]) {
 			return false;
 		}		
 	}
 	return true;
 }
 
-function filterCause(element) {
-	if (GLOBAL.filters.cause == element.label){
-		GLOBAL.filters.cause = null
-	} else {
-		GLOBAL.filters.cause = element.label
+function applyFilter(category) {
+	return function(element) {
+		if (GLOBAL.categories[category].filter == element.label){
+			GLOBAL.categories[category].filter = null
+		} else {
+			GLOBAL.categories[category].filter = element.label
+		}
+		draw(GLOBAL.svg);
 	}
-	draw(GLOBAL.svg);
 }
